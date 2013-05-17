af463bdf4ce33c1efd395413f05f03272be047a4
Yusuke Ando <yandod@gmail.com>
2013-05-17 17:43:41 +0900

    up

--- before_bundle.rb
+++ before_bundle.rb
@@ -1,10 +1,20 @@
 # html/webapp/config/install.inc.php
 if !File.exist?(current_path + "/html/webapp/config/install.inc.php") then
+  #config
   run "cp #{current_path}/html/webapp/config/install.inc.php #{shared_path}/config/install.inc.php"
+  # images js themes
+  run "cp -R #{current_path}/html/htdocs/images #{shared_path}/htdocs/images"
+  run "cp -R #{current_path}/html/htdocs/js #{shared_path}/htdocs/js"
+  run "cp -R #{current_path}/html/htdocs/themes #{shared_path}/htdocs/themes"
 end
 
 if File.exist?(shared_path + "/config/install.inc.php") then
+  run "rm #{release_path}/html/webapp/config/install.inc.php"
   run "ln -s #{shared_path}/config/install.inc.php #{release_path}/html/webapp/config/install.inc.php"
+  # images js themes
+  run "ln -s #{shared_path}/htdocs/images #{release_path}/html/htdocs/images"
+  run "ln -s #{shared_path}/htdocs/js #{release_path}/html/htdocs/js"
+  run "ln -s #{shared_path}/htdocs/themes #{release_path}/html/htdocs/themes"
 end
 
 # html/webapp/templates_c
